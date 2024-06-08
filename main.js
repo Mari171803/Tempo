c05d7a593e64c3394828cc106d46f4acf8ab9dc1
Mari171803 <marianasantos151803@gmail.com>
2024-06-08 12:04:02 -0300

    Conclusão: adicionado sensação térmica, velocidade dos ventos, cidade e estilização simples. Feito no dia 08 de junho de 2024. (●'◡'●)

--- main.js
+++ main.js
@@ -19,10 +19,14 @@ window.addEventListener('load', () => {
 
             console.log(latitude + '-' + longitude)
 
+
             const getWeather = async () => {
                 const getData = await fetch(`https://api.weatherapi.com/v1/current.json?key=6b97fe193a374eb09d6121028240106&q=${latitude},${longitude}&lang=pt`)
 
                 const res = await getData.json()
+
+                const condicoescontainer = document.querySelector('.condicoes')
+                const localatualcontainer = document.querySelector('.local-atual')
 
                 console.log(res.location.name) //lugar
                 console.log(res.location.region) //estado
@@ -32,26 +36,35 @@ window.addEventListener('load', () => {
                 console.log(res.current.humidity) //humidade
                 console.log(res.current.wind_kph) //ventos em km por hora
 
+                const icone = document.createElement('img')
                 const condicao = document.createElement('p')
+                const cidade = document.createElement('p')
+                const temp = document.createElement('p')
+                const senstermica = document.createElement('p')
+                const velocvento = document.createElement('p')
 
                 condicao.innerHTML = res.current.condition.text
-                document.querySelector('.dados').appendChild(condicao)
+                icone.setAttribute('src', 'https:'+res.current.condition.icon)
+                temp.innerHTML = res.current.temp_c
+                cidade.innerHTML = res.location.name
+                senstermica.innerHTML = res.current.feelslike_c
+                velocvento.innerHTML = res.current.wind_kph + " km/h"
 
-                condicao.innerHTML = res.current.temp_c
-                document.querySelector('.dados').appendChild(condicao)
+                temp.classList.add('temperatura')
+                cidade.className = 'cidade'
+                
+                condicoescontainer.appendChild(icone)
+                condicoescontainer.appendChild(condicao)
+                condicoescontainer.appendChild(temp)
 
-                condicao.innerHTML = res.current.feelslike_c
-                document.querySelector('.dados').appendChild(condicao)
-
-                condicao.innerHTML = res.current.humidity
-                document.querySelector('.dados').appendChild(condicao)
-
-                condicao.innerHTML = res.current.wind_kph
-                document.querySelector('.dados').appendChild(condicao)
-
+                localatualcontainer.appendChild(senstermica)
+                localatualcontainer.appendChild(velocvento)
+                localatualcontainer.appendChild(cidade)
+                
             }
             getWeather()
         })
+        document.querySelector('.load').className = 'hideLoad'
     }
     getNavigator()
 })
